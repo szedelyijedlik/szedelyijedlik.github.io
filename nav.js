f851844730d1fcc4e0dbbab9e3c57b4b1d86785f
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2022-11-04 17:22:05 +0100

    v0.8 js nav, ID.Buzz page

---
+++ nav.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    var menu = document.querySelector(".hamburger")
+    menu.style.right = 0
+}
+
+function closeMenu(){
+    var menu = document.querySelector(".hamburger")
+    menu.style.right = "-70%"
+}
+

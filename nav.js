9f4ab662364673f102cdf0ccfeaeb23c2df642bc
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2022-11-05 15:07:32 +0100

    v0.95 fix, mute btn

--- nav.js
+++ nav.js
@@ -8,3 +8,11 @@ function closeMenu(){
     menu.style.right = "-70%"
 }
 
+function mute(){
+    var video = document.querySelector("video")
+    if (video.muted === false) {
+        video.muted = true;
+    } else {
+        video.muted = false;
+    }
+}
